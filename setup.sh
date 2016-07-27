6abe87b4353690c3afd59d6f170ef817bcf029e5
rdiverdi <Rocco.DiVerdi@students.olin.edu>
2016-07-27 18:49:58 +0000

    fixed shell script call

--- setup.sh
+++ setup.sh
@@ -1,5 +1,5 @@
 export PATH=$PATH:~/Odroid_Setup
 source ~/Odroid_Setup/auto_set_ros_vars.sh
-setup_aliases.sh
+source setup_aliases.sh
 alias set_ros_vars_auto='source ~/Odroid_Setup/auto_set_ros_vars.sh'
 
