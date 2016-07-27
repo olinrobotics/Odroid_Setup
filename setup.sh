1dc615160d5ec40d94915cd268e803f9fca79d45
rdiverdi <Rocco.DiVerdi@students.olin.edu>
2016-07-27 14:03:06 -0400

    added convenience scripts

---
+++ setup.sh
@@ -0,0 +1,3 @@
+export PATH=$PATH:~/Odroid_Setup
+source ~/Odroid_Setup/auto_set_ros_vars.sh
+setup_aliases.sh
---
+++ setup_aliases.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+alias set_ros_master_uri='source ~/Odroid_Setup/set_ros_master_uri.sh'
+alias set_ros_vars_auto='source ~/Odroid_Setup/auto_set_ros_vars.sh'
+alias set_ros_vars='source ~/Odroid_Setup/set_ros_variables.sh'
